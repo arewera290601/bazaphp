e15e495c26dcae61788e3fb1873ee50fb6a7037b
arewera290601 <aleksandrarewera@gmail.com>
2019-11-26 07:24:02 +0100

    ds

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,11 @@
 <?php
 
+class Baza {
+	private $db = null;
+	var $ret = array();
+	var $mode = PDO::FETCH_ASSOC;
+}
+
 function init_baza() {
 	global $db, $dbfile, $kom;
 	if(!file_exists($dbfile))
@@ -7,7 +13,37 @@ function init_baza() {
 
 	$db = new PDO("sqlite:$dbfile");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 }
+
+function init_tables(){
+	global $db $kom;
+	if(file_exists('baza/baza.sql')){
+		$q = "SELECT name FROM sqlite_master WHERE type='table' AND name='menu'";
+		$ret = array();
+		db_query($q, $ret);
+		if (empty($ret)) {
+			$sql = file_get_contents('baza/baza.sql');
+			$db->exec($sql);
+			$kom[] = "Utworzono tabele!";
+		}
+	}
+}
+
+function db_query($q, &$ret) {
+	global $db;
+	$r = null;
+	try {
+		$r = $db->query($q);
+	}
+	catch(PDOException $e) {
+		echo ($e->getMessage());
+	}
+	if ($r) {
+		$ret = $r->fetchAll();
+		return true;
+	}
+	return false;
+}
+
 
 ?>
